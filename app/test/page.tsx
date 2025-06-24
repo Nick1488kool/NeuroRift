1e1de063118abe2251418de60d459b1bd194cd00
Nick <nick.creator.ai@gmail.com>
2025-06-24 16:44:44 +0300

    Подключены компоненты, добавлена страница теста

---
+++ app/test/page.tsx
@@ -0,0 +1,7 @@
+export default function TestPage() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h2 className="text-3xl font-bold">Здесь будет тест</h2>
+    </div>
+  );
+}
